64c4e86a31fb25c992aad055fcf042936f1063e1
matin <matinnjt2000@gmail.com>
2025-01-24 15:46:51 +0330

    Base User setup with profile

---
+++ core/account/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from account.models import User, Profile
+
+"""
+save a profile for the created user
+"""
+@receiver(post_save, sender=User)
+def create_profile_for_user(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(
+            user=instance,
+        )
--- core/core/settings.py
+++ core/core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom user model
+AUTH_USER_MODEL = "account.User"
+
+#Media files settings
+MEDIA_ROOT = "media/"
+MEDIA_URL = "media/"
