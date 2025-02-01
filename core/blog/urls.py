651af0ce60fce0323098277c95abcc43e4e2059a
matin <matinnjt2000@gmail.com>
2025-02-01 13:50:14 +0330

    blog category search result for posts done

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path , include
+from . import views
 app_name = 'blog'
 urlpatterns = [
     path('api/v1/', include('blog.api.v1.urls')),
+    path('category-result/<str:category_name>' , views.CategoryResultTemplateView.as_view() , name='category-result')
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Category , Post
+from django.db.models import Count
 
-# Create your views here.
+class CategoryResultTemplateView(ListView):
+    template_name = 'blog/category_result_page/main.html'
+    paginate_by = 5
+    context_object_name = 'category_posts'
+    page_kwarg = "page"
+    ordering = '-created_at'
+    def get_queryset(self):
+        category_name = self.kwargs.get('category_name')
+        category_posts = Category.objects.get(name=category_name).posts.select_related('author').all()
+        return category_posts
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category_name'] = self.kwargs.get('category_name')
+        context['popular_posts'] = Post.objects.annotate(num_comments=Count('comments')).order_by('-num_comments')[:3]
+        context['categories'] = Category.objects.annotate(num_posts=Count('posts')).order_by('-num_posts')
+        return context
