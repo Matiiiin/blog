31fd0629c25c952136662da0c7a0042286594ce5
matin <matinnjt2000@gmail.com>
2025-02-01 18:03:25 +0330

    blog post details page including comments , author , other posts , categories , main content done

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('api/v1/', include('blog.api.v1.urls')),
-    path('category-result/<str:category_name>' , views.CategoryResultTemplateView.as_view() , name='category-result')
+    path('category-result/<str:category_name>' , views.CategoryResultTemplateView.as_view() , name='category-result'),
+    path('post/<slug:slug>' , views.PostDetailView.as_view() , name='post-detail'),
+    path('comment/<int:comment_id>/comment-reply/create/' , views.CommentReplyView.as_view() , name='comment-reply-create'),
+    path('post/<slug:post_slug>/comment/create' , views.CommentView.as_view() , name='comment-create'),
 ]
